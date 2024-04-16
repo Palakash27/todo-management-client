ec384af7fd616e0bda926db734d02bfe7a0c00e5
Aakash Pal <palakash27@gmail.com>
2024-04-16 01:45:14 -0400

    Migrated from CRA to Vite

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,16 +1,31 @@
+import Avatar1 from "../assets/avatars/avatar-1.jpg";
+import Avatar10 from "../assets/avatars/avatar-10.jpg";
+import Avatar12 from "../assets/avatars/avatar-12.jpg";
+import Avatar13 from "../assets/avatars/avatar-13.jpg";
+import Avatar14 from "../assets/avatars/avatar-14.jpg";
+import Avatar15 from "../assets/avatars/avatar-15.jpg";
+import Avatar16 from "../assets/avatars/avatar-16.jpg";
+import Avatar17 from "../assets/avatars/avatar-17.jpg";
+import Avatar18 from "../assets/avatars/avatar-18.jpg";
+import Avatar2 from "../assets/avatars/avatar-2.jpg";
+import Avatar3 from "../assets/avatars/avatar-3.jpg";
+import Avatar4 from "../assets/avatars/avatar-4.jpg";
+import Avatar5 from "../assets/avatars/avatar-5.jpg";
+import Avatar7 from "../assets/avatars/avatar-7.jpg";
+
 export const IMAGES = {
-    Avatar1: "/static/media/avatar-1.2bcd9da29105cdf93ed1.jpg", // ../assets/avatars/avatar-1.jpg
-    Avatar2: "/static/media/avatar-2.41e9a621fcbfb127350c.jpg", // ../assets/avatars/avatar-2.jpg
-    Avatar3: "/static/media/avatar-3.5959b0b50637efc0f66b.jpg", // ../assets/avatars/avatar-3.jpg
-    Avatar4: "/static/media/avatar-4.d15f6df5fc2d53100143.jpg", // ../assets/avatars/avatar-4.jpg
-    Avatar5: "/static/media/avatar-5.c2ec7908ce27cd41738b.jpg", // ../assets/avatars/avatar-5.jpg
-    Avatar6: "/static/media/avatar-7.74ae570538214672ef9c.jpg", // ../assets/avatars/avatar-7.jpg
-    Avatar7: "/static/media/avatar-10.716de0c5d7299c49f11c.jpg", // ../assets/avatars/avatar-10.jpg
-    Avatar8: "/static/media/avatar-12.218d0109e1c74f6431f0.jpg", // ../assets/avatars/avatar-12.jpg
-    Avatar9: "/static/media/avatar-13.4e6f30f43565c03b3143.jpg", // ../assets/avatars/avatar-13.jpg
-    Avatar10: "/static/media/avatar-14.1393aa3d8c7770612f18.jpg", // ../assets/avatars/avatar-14.jpg
-    Avatar11: "/static/media/avatar-15.30726fdbeaafcb21db5c.jpg", // ../assets/avatars/avatar-15.jpg
-    Avatar12: "/static/media/avatar-16.a83042f16842af044954.jpg", // ../assets/avatars/avatar-16.jpg
-    Avatar13: "/static/media/avatar-17.f4fe52c258c6e3f1cede.jpg", // ../assets/avatars/avatar-17.jpg
-    Avatar14: "/static/media/avatar-18.13def8c83cf2c0a81e04.jpg", // ../assets/avatars/avatar-18.jpg
+    Avatar1,
+    Avatar2,
+    Avatar3,
+    Avatar4,
+    Avatar5,
+    Avatar7,
+    Avatar10,
+    Avatar12,
+    Avatar13,
+    Avatar14,
+    Avatar15,
+    Avatar16,
+    Avatar17,
+    Avatar18,
 };
